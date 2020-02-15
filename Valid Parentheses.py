425ae6f271be5f9881a944c002b209028ca2e538
devbodaghe <40855726+devbodaghe@users.noreply.github.com>
2020-02-15 19:04:46 +0100

    Leetcode 20

---
+++ Valid Parentheses.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#Valid Parentheses
+
+
+# In[5]:
+
+
+class Solution:
+
+    
+    def isValid(self, s: str) -> bool:
+        
+        stack = []
+        dict = {"]":"[", "}":"{", ")":"("}
+        for char in s:
+            if char in dict.values():
+                stack.append(char)
+            elif char in dict.keys():
+                if (stack ==[])  or (dict[char] != stack.pop()):
+                    return False
+            else:
+                return False
+        return stack == []
+
+
+# In[ ]:
+
+
+
+
